b6ec20a82c4313921d853992c275ac7b0a8ad295
yiwen2577 <yiwen2577@gmail.com>
2025-03-07 12:45:34 +0000

    okdfd

--- app.py
+++ app.py
@@ -6,5 +6,23 @@ app = Flask(__name__)
 def index():
     return (render_template("index.html"))
 
+@app.route("/main",methods = ["POST","GET"])
+def main():
+    user_name = request.form.get("q")
+    return (render_template("main.html"))
+
+@app.route("/ethical_test",methods = ["POST","GET"])
+def ethical_test():
+    return (render_template("ethical_test.html"))
+
+@app.route("/test_result",methods = ["POST","GET"])
+def test_result():
+    answer = request.form.get("answer")
+    if answer == "false":
+        return (render_template("pass.html"))
+    elif answer == "true":
+        return (render_template("fail.html"))
+
+
 if __name__=="__main__":
     app.run()
