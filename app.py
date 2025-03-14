ba8acc6e9af530a4608910bf47ffede078e9aca2
yiwen2577 <yiwen2577@gmail.com>
2025-03-14 11:53:13 +0000

    1

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ def main():
     user_name = request.form.get("q")
     return (render_template("main.html"))
 
+@app.route("/foodexp",methods = ["POST","GET"])
+def foodexp():
+    return (render_template("foodexp.html"))
+
+@app.route("/foodexp_pred",methods = ["POST","GET"])
+def foodexp_pred():
+    q = float(request.form.get("q")) #float 很重要
+    return (render_template("foodexp_pred.html",r=q*0.4851+147.4))
+
 @app.route("/ethical_test",methods = ["POST","GET"])
 def ethical_test():
     return (render_template("ethical_test.html"))
